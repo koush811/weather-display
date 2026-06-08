83c05ef78a02887a5cac718504d9001174247b01
kosei <123241044+koush811@users.noreply.github.com>
2026-06-08 16:47:50 +0900

    table

--- weather-display/src/App.jsx
+++ weather-display/src/App.jsx
@@ -2,6 +2,7 @@ import Weather from "./frontend/components/Weather";
 import Clock from "./frontend/components/Clock"
 import Alert from "./frontend/components/Alert"
 import Wbgt from "./frontend/components/wbgt"
+import Table from "./frontend/components/table";
 import "./app.css"
 
 function app(){
@@ -15,6 +16,9 @@ function app(){
             </div>
             <div className="dev Alert">
                 <Alert></Alert>
+            </div>
+            <div className="dev">
+                <Table></Table>
             </div>
         </>
     )
