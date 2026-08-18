fc0e335d6476ca60616550e9aa5216c30cd64e4e
kosei <123241044+koush811@users.noreply.github.com>
2026-08-18 09:54:03 +0900

    表変更

--- weather-display/src/frontend/components/table.jsx
+++ weather-display/src/frontend/components/table.jsx
@@ -1,51 +1,58 @@
 import "../components.css"
 
-function Table(){
-    return(
+function Table() {
+    return (
         <>
-            
-        <h2>暑さ指数（WBGT）</h2>
 
-           <table>
+            <h2>暑さ指数（WBGT）</h2>
+
+            <table>
                 <thead>
                     <tr>
                         <th>暑さ指数（WBGT）</th>
-                        <th>注意すべき生活活動の目安</th>
-                        <th>注意事項</th>
+                        <th>予防策</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                    <td className="kiken">危険（31以上）</td>
-                    <td>すべての生活活動で起こる危険性</td>
-                    <td>
-                      高齢者においては安静状態でも発生する危険性が大きい。
-                      外出はなるべく避け、涼しい室内に移動する。
-                    </td>
-                    </tr>
-                    <tr>
-                        <td className="genjyu">厳重警戒（28以上31未満）</td>
-                        <td>すべての生活活動で起こる危険性</td>
+                        <td className="special-alert">危険（35以上）</td>
                         <td>
-                        外出時は炎天下を避け、室内では室温の上昇に注意する。
+                            熱中症特別警戒アラート
                         </td>
                     </tr>
                     <tr>
-                        <td className="keikai">警戒（25以上28未満）</td>
-                        <td>中東以上の生活活動で起こる危険性</td>
+                        <td className="nomal-alert">危険（33以上）</td>
                         <td>
-                        運動や激しい作業をする際は定期的に充分に休息を取り入れる。
+                            熱中症警戒アラート
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="kiken">危険（31以上）</td>
+                        <td>
+                            原則 運動は中止
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="genjyu">厳重警戒（28~31）</td>
+                        <td>
+                            激しい運動は中止
+                        </td>
+                    </tr>
+                    <tr>
+                        <td className="keikai">警戒（25~28）</td>
+                        
+                        <td>
+                            積極的に休憩をとる
                         </td>
                     </tr>
                     <tr>
                         <td className="tyuui">注意（25以下）</td>
-                        <td>強い生活活動で起こる危険性</td>
                         <td>
-                        一般に危険性は少ないが激しい運動や重労働時には発生する危険性がある。
+                            積極的に水分をとる
                         </td>
                     </tr>
                 </tbody>
-            </table> 
+            </table>
         </>
     )
 }
